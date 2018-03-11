2944adc992fcd1f93b3f8841f14cb01a81f32901
AgafonovK <konstantinagaf@gmail.com>
2018-03-11 21:40:35 +0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+public class Controller {
+
+    @FXML
+    private BorderPane borderPane;
+
+
+    @FXML
+    private void ui1(MouseEvent event) throws Exception {
+
+        Parent root = null;
+        root = FXMLLoader.load(getClass().getResource("/sample2.fxml"));
+
+        borderPane.setCenter(root);
+    }
+}
